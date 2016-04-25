dd6796d57fa9195af623dab0c3e2f45d7268dede
Yusuke Ohashi <github@junkpiano.me>
2016-04-26 08:10:37 +0900

    removed unnecesary files.

--- Example/Pods/Target Support Files/HOBProgressHUD/HOBProgressHUD-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HOBProgressHUD : NSObject
-@end
-@implementation PodsDummy_HOBProgressHUD
-@end
--- Example/Pods/Target Support Files/Pods-HOBProgressHUD_Example/Pods-HOBProgressHUD_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_HOBProgressHUD_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HOBProgressHUD_ExampleVersionString[];
-
